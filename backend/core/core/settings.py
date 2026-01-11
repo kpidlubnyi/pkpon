a52873e0a84d873a46434f9ab17d6893bee3ccad
kpidlubnyi <kpidlubnyi@gmail.com>
2026-01-11 15:41:00 +0100

    feat: add CORS

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'django_celery_beat',
+    'corsheaders',
 
     'stops',
     'tasks',
@@ -28,7 +29,12 @@ INSTALLED_APPS = [
     'trips',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
+]
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
