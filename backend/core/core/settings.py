9dfaba41fb4e41d8da1652c27062c70588a1960d
kpidlubnyi <kpidlubnyi@gmail.com>
2026-01-15 19:12:24 +0100

    chore: add more CORS configuration

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -29,9 +29,29 @@ INSTALLED_APPS = [
     'trips',
 ]
 
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173'
 ]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = [
+    "content-type",
+    "authorization",
+    "x-csrftoken",
+    "accept",
+    "origin",
+    "user-agent",
+    "x-requested-with",
+]
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "OPTIONS",
+]
+
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
