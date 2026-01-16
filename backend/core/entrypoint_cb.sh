d9d4bada627c65ead06889925d5ec136c4faf7a5
kpidlubnyi <kpidlubnyi@gmail.com>
2026-01-16 15:00:16 +0100

    fix: delete entrypoints and restore the previous state of container startup

--- backend/core/entrypoint_cb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-
-exec celery -A core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler
--- backend/core/entrypoint_cw.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-
-exec celery -A core worker --loglevel=info
