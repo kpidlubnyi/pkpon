9616068b49cc3992dcc64f45c5069c902f9a4f61
kpidlubnyi <kpidlubnyi@gmail.com>
2026-01-29 16:38:30 +0100

    feat: add creating initial isochrone cache task

--- backend/core/tasks/tasks.py
+++ backend/core/tasks/tasks.py
@@ -1,10 +1,15 @@
 from celery import shared_task
+from time import sleep
+import requests
 import logging
 import os
 
 from django.conf import settings
+from django.db.models import Count
 
-from core.services.redis import set_hash, truncate_gtfs_related_cached_data, get_hash
+from core.services.redis import set_hash, truncate_gtfs_related_cached_data
+from stops.services.views import get_isochrone_map
+from tasks.models import StopTime
 from tasks.services.gtfs.download import FeedGTFS
 from tasks.services.gtfs.db import (
     backup_from_common_tables, recreate_data_in_complete_trips,
@@ -52,7 +57,8 @@ def check_gtfs_update():
         logger.info("Backing up production tables to staging...")
         backup_from_common_tables()
 
-        logger.info("GTFS import task completed successfully")
+        logger.info("GTFS import task completed successfully. Calling the isochrone creating task...")
+        create_isochrone_cache.delay()
         return {"status": "ok", "sha": feed.sha}
 
     except Exception as e:
@@ -74,3 +80,51 @@ def update_orr_map():
         return {'created': True, 'path':flag_path}
     except Exception as e:
         return {'created': False, 'error': e}
+
+
+@shared_task
+def create_isochrone_cache():
+    logger.info('Creating isochrone cache...')
+    created_layers = 0
+    qs = (
+        StopTime.objects
+        .values('stop__stop_id', 'stop__stop_name')
+        .annotate(trip_id=Count('trip_id'))
+        .order_by('-trip_id')
+    )
+
+    limits = [
+        (5,   5),
+        (20,  4),
+        (100, 3),
+        (250, 2),
+    ]
+
+    healthcheck_url = f'{settings.ORR_URL}/health'
+    for attempt in range(1, 61):
+        try:
+            requests.get(healthcheck_url, timeout=5).raise_for_status()
+            break
+        except requests.RequestException as e:
+            logger.warning(
+                f"ORR Healthcheck failed (attempt {attempt}/60): {e}"
+            )
+            sleep(5)
+    else:
+        raise RuntimeError("ORR service is not healthy after max retries")
+
+
+    for i, el in enumerate(qs.iterator(), start=1):
+        stop_id = el['stop__stop_id']
+        for limit, level in limits:
+            if i <= limit:
+                _ = get_isochrone_map(stop_id, level)
+                created_layers += 1
+                break
+        else:
+            _ = get_isochrone_map(stop_id, 1)
+            created_layers += 1
+
+        logger.info(f"Created isochrone cache for the stop with id '{stop_id}' with {level} priority level.")
+    
+    return {'status': 'ok', 'created_layers': created_layers}
